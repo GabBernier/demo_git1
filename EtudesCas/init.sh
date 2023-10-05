ef01bd1e3e3cfbcbe99c980574b3fa86e7f623e2
Gabriel Bernier <gaber154@ulaval.ca>
2023-10-05 11:09:09 -0400

    Ajout du script init.sh
    Le but de se commit est d'ajouter le script init.sh dans la base
    de donnée sur GitHub dans le répertoire EC1_etudiant02.

---
+++ EtudesCas/init.sh
@@ -0,0 +1,8 @@
+ #!/bin/bash
+
+mkdir data/ analyses/ résultats/ scripts/
+
+ln -sf /home/public/EtudeCas1/raw_data/
+ln -sf /home/public/rna_seq/reference/
+
+cp /home/public/rna_seq/demo/adaptors.fa data/adaptors/
